b1bf4f2ba0dab06a792ee37b19c16bb906dde48a
Tomilade18 <jboyyumzey@gmail.com>
2026-06-03 23:01:08 +0100

    first commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,18 @@
+
+
+const register = async (req, res) => {
+
+    const {name, email, password} = req.body;
+
+    
+};
+
+const login = async (req, res) => {
+
+};
+
+const logout = async (req, res) => {
+
+}
+
+export {register, login, logout}
